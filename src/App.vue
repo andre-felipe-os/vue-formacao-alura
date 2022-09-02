36b6130887888c53e0ebbdd707d80007f04518ea
André Sousa <andre.lipe.o@gmail.com>
2022-09-02 04:56:45 -0300

    feat: utiliza componente Formulário em app

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,22 @@
     <div class="column is-one-quarter">
       <BarraLateralVue />
     </div>
+    <div class="column is-three-quarter">
+      <FormularioVue />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import BarraLateralVue from './components/BarraLateral.vue';
+  import FormularioVue from './components/Formulario.vue';
 
   export default defineComponent({
     name: 'App',
     components: {
-      BarraLateralVue
+      BarraLateralVue,
+      FormularioVue
     }
   });
 </script>
