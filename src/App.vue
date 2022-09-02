2ee7331750d430c4864e75733114d598569ebf5b
André Sousa <andre.lipe.o@gmail.com>
2022-09-02 04:55:21 -0300

    feat: utiliza o componente BarraLateral no app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateralVue />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+  import { defineComponent } from 'vue';
+  import BarraLateralVue from './components/BarraLateral.vue';
 
-export default defineComponent({
-  name: 'App'
-});
+  export default defineComponent({
+    name: 'App',
+    components: {
+      BarraLateralVue
+    }
+  });
 </script>
 
 <style>
