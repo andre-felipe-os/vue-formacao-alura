cc6c8d31cb674963a0d4b24b08840272f76b28f2
André Sousa <andre.lipe.o@gmail.com>
2022-09-04 10:31:34 -0300

    feat: adiciona lista de tarefas ao app
    Adiciona um array de tarefas na propriedade 'data' no App e mostra cada tarefa usando v-for e o componente Tarefa.

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,32 @@
     </div>
     <div class="column is-three-quarter">
       <FormularioVue />
+      <div class="lista">
+        <TarefaVue
+            v-for="(tarefa, indice) of tarefas"
+            v-bind:key="indice"
+            v-bind:tarefa="tarefa" />
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
+  import TarefaInterface from './interfaces/TarefaInterface';
   import BarraLateralVue from './components/BarraLateral.vue';
   import FormularioVue from './components/Formulario.vue';
+  import TarefaVue from './components/Tarefa.vue';
 
   export default defineComponent({
     name: 'App',
+    data() {
+      return {
+        tarefas: new Array<TarefaInterface>()
+      }
+    },
     components: {
+      TarefaVue,
       BarraLateralVue,
       FormularioVue
     }
