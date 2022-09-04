1fdcc5f3587bc0b83ab889133b0313240cd7a8e0
André Sousa <andre.lipe.o@gmail.com>
2022-09-04 08:50:46 -0300

    feat: adiciona componente Tarefa
    O componente Tarefa recebe um objeto que segue a interface Tarefa e exibe esse item.

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao }}
+      </div>
+      <div class="column">
+        <CronometroVue :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType } from "vue";
+  import TarefaInterface from "../interfaces/TarefaInterface";
+  import CronometroVue from "./Cronometro.vue";
+
+  export default defineComponent({
+      name: 'TarefaVue',
+      components: {
+          CronometroVue
+      },
+      props: {
+        tarefa: {
+          type: Object as PropType<TarefaInterface>,
+          required: true
+        }
+      }
+  })
+</script>
+
+<style scoped>
+  .box {
+    background-color: #FAF0CA;
+  }
+</style>
