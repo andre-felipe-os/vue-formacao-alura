1db5247ad4ae64754b04bd0e006e983defa007e2
André Sousa <andre.lipe.o@gmail.com>
2022-09-05 19:37:13 -0300

    feat(tema-escuro): ativa cores temáticas no componente Formulário

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box">
+  <div class="box formulario">
     <div class="columns">
       <div class="column is-8"
           role="form"
@@ -43,3 +43,10 @@
     }
   })
 </script>
+
+<style>
+  .formulario {
+    background-color: var(--fundo-primario);
+    color: var(--texto-primario);
+  }
+</style>
