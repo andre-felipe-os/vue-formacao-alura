5f25cf810b9d2514e38f254a15d9b1ae556dde6e
André Sousa <andre.lipe.o@gmail.com>
2022-09-09 20:17:45 -0300

    feat: adiciona formulário de inserção de projeto

--- src/views/ProjetosView.vue
+++ src/views/ProjetosView.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Projeto!</h1>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form>
+      <div class="field">
+        <label for="nomeDoProjeto" class="label">
+          Nome do projeto
+        </label>
+        <input
+            type="text"
+            class="input"
+            id="nomeDoProjeto"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">
+          Salvar
+        </button>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script lang="ts">
