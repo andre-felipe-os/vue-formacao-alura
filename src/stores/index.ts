ceb6934c26403ff207076372c84552afa078a856
André Sousa <andre.lipe.o@gmail.com>
2022-09-12 18:37:51 -0300

    feat: configura chave de acesso à store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,9 +1,12 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
 import ProjetoInterface from "../interfaces/ProjetoInterface";
 
 interface EstadoInterface {
     projetos: Array<ProjetoInterface>;
 }
+
+export const key: InjectionKey<Store<EstadoInterface>> = Symbol();
 
 export const store = createStore<EstadoInterface>({
     state: {
