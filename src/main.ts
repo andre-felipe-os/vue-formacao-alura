b32929d2ca32e1ed7a016aec901f3869b2105649
André Sousa <andre.lipe.o@gmail.com>
2022-09-13 20:42:58 -0300

    fix: configura aplicação para utilizar a store criada

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 import roteador from './routers'
+import { key, store } from './stores'
 
 createApp(App)
     .use(roteador)
+    .use(store, key)
     .mount('#app')
