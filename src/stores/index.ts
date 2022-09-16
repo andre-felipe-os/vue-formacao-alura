02e698df90629c24689661096b11ae4cc895fb47
André Sousa <andre.lipe.o@gmail.com>
2022-09-16 12:34:46 -0300

    feat: adiciona mutation 'ADICIONA_PROJETO'

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -11,5 +11,14 @@ export const key: InjectionKey<Store<EstadoInterface>> = Symbol();
 export const store = createStore<EstadoInterface>({
     state: {
         projetos: Array<ProjetoInterface>()
+    },
+    mutations: {
+        'ADICIONA_PROJETO'(state, nomeDoProjeto: string): void {
+            const projeto = {
+                id: new Date().toISOString(),
+                nome: nomeDoProjeto
+            } as ProjetoInterface;
+            state.projetos.push(projeto);
+        }
     }
 });
