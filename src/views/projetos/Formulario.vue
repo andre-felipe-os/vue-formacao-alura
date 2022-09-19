06c175c6f7c4167140f8c3b67ac9995f8f8592bf
André Sousa <andre.lipe.o@gmail.com>
2022-09-19 09:52:22 -0300

    feat: extrai formulário de cadastro de Projetos para visão 'Formulário'

---
+++ src/views/projetos/Formulario.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar" class="columns">
+      <div class="field column is-10">
+        <label for="nomeDoProjeto" class="label">
+          Nome do projeto
+        </label>
+        <input
+            type="text"
+            class="input"
+            id="nomeDoProjeto"
+            v-model="nomeDoProjeto"
+        />
+      </div>
+      <div class="field column is-2" style="position: relative;">
+        <button class="button is-primary" type="submit" style="width: 80%; position: absolute; left: 0; bottom: 0; margin-left: 15px; margin-bottom: 24px; margin-right: 20px;">
+          Salvar
+        </button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  import { useStore } from "@/stores";
+
+  export default defineComponent({
+    name: 'FormularioView',
+    data() {
+      return {
+        nomeDoProjeto: '',
+      }
+    },
+    setup() {
+      const store = useStore();
+      return {
+        store
+      };
+    },
+    methods: {
+      salvar(): void {
+        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
+        this.nomeDoProjeto = '';
+      }
+    }
+  });
+</script>
+
+<style scoped>
+  .projetos {
+    padding: 1.25rem;
+  }
+</style>
