52ecad47fdae0b284420801854b0f1c680dc5882
André Sousa <andre.lipe.o@gmail.com>
2022-09-19 14:03:53 -0300

    feat: reconfigura método de salvar um projeto
    Caso exista um id para aquele projeto, apenas altera esse projeto existente. Caso contrário, adiciona um novo projeto, com id próprio.

--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -54,7 +54,14 @@
     },
     methods: {
       salvar(): void {
-        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
+        if (this.id) {
+          this.store.commit('ALTERA_PROJETO', {
+            id: this.id,
+            nome: this.nomeDoProjeto
+          });
+        } else {
+          this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
+        }
         this.nomeDoProjeto = '';
         this.$router.push('/projetos');
       }
