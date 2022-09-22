a5e303fad80e7e3ded7106ff04e451a5a0377ece
André Sousa <andre.lipe.o@gmail.com>
2022-09-22 20:53:04 -0300

    feat: apresenta notificações a partir do array no estado da aplicação

--- src/components/Notificacoes.vue
+++ src/components/Notificacoes.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="notificacoes">
-    <article class="notification">
+    <article class="notification"
+        v-for="notificacao of notificacoes"
+        v-bind:key="notificacao.id">
       <button class="delete"></button>
-      Texto da notificação.
+      {{ notificacao.texto }}
     </article>
   </div>
 </template>
 
 <script lang="ts">
+import { useStore } from '@/stores';
 import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'NotificacoesVue'
+  name: 'NotificacoesVue',
+  setup() {
+    const store = useStore();
+    return {
+        notificacoes: computed(() => store.state.notificacoes)
+    }
+  }
 });
 </script>
 
