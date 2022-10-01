b0941130ac60de2311f897a2daa6aa8f0c455ddc
André Sousa <andre.lipe.o@gmail.com>
2022-10-01 19:35:29 -0300

    refactor: extrai mutations e ações de projetos para um módulo

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,14 +1,13 @@
 import clienteHttp from "@/http";
 import { NotificacaoInterface } from "@/interfaces/NotificacaoInterface";
 import TarefaInterface from "@/interfaces/TarefaInterface";
-import { AxiosResponse } from "axios";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import ProjetoInterface from "../interfaces/ProjetoInterface";
-import { ALTERAR_PROJETO, ALTERAR_TAREFA, CADASTRAR_PROJETO, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETO } from "./tipos-de-acoes";
-import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUI_PROJETO, NOTIFICAR, REMOVE_NOTIFICACAO } from "./tipos-de-mutacoes";
+import { ALTERAR_TAREFA, CADASTRAR_TAREFA, OBTER_TAREFAS } from "./tipos-de-acoes";
+import { ADICIONA_TAREFA, ALTERA_TAREFA, DEFINIR_TAREFAS, NOTIFICAR, REMOVE_NOTIFICACAO } from "./tipos-de-mutacoes";
 
-interface EstadoInterface {
+export interface EstadoInterface {
     tarefas: Array<TarefaInterface>;
     projetos: Array<ProjetoInterface>;
     notificacoes: Array<NotificacaoInterface>;
@@ -23,25 +22,6 @@ export const store = createStore<EstadoInterface>({
         projetos: Array<ProjetoInterface>()
     },
     mutations: {
-        [ADICIONA_PROJETO](state, nomeDoProjeto: string): void {
-            const projeto = {
-                id: new Date().toISOString(),
-                nome: nomeDoProjeto
-            } as ProjetoInterface;
-            state.projetos.push(projeto);
-        },
-        [ALTERA_PROJETO](state, projetoAlterado: ProjetoInterface): void {
-            const index = state.projetos
-                .findIndex(projeto => projeto.id == projetoAlterado.id);
-            state.projetos[index] = projetoAlterado;
-        },
-        [EXCLUI_PROJETO](state, idDoProjeto: string): void {
-            state.projetos = state.projetos
-                .filter(projeto => projeto.id != idDoProjeto);
-        },
-        [DEFINIR_PROJETOS](state, projetos: Array<ProjetoInterface>): void {
-            state.projetos = projetos;
-        },
         [NOTIFICAR](state, novaNotificacao: NotificacaoInterface) {
             novaNotificacao.id = new Date().getTime();
             state.notificacoes.push(novaNotificacao);
@@ -69,22 +49,6 @@ export const store = createStore<EstadoInterface>({
         },
     },
     actions: {
-        [OBTER_PROJETOS]({ commit }) {
-            clienteHttp.get("projetos")
-                .then(resposta => commit(DEFINIR_PROJETOS, resposta.data));
-        },
-        [CADASTRAR_PROJETO](contexto, nomeDoProjeto: string): Promise<AxiosResponse> {
-            return clienteHttp.post("/projetos", {
-                nome: nomeDoProjeto,
-            });
-        },
-        [ALTERAR_PROJETO](contexto, projeto: ProjetoInterface): Promise<AxiosResponse> {
-            return clienteHttp.put(`/projetos/${projeto.id}`, projeto);
-        },
-        [REMOVER_PROJETO]({ commit }, idDoProjeto: string) {
-            return clienteHttp.delete(`/projetos/${idDoProjeto}`)
-                .then(() => commit(EXCLUI_PROJETO, idDoProjeto));
-        },
         [OBTER_TAREFAS]({ commit }) {
             clienteHttp.get("tarefas")
                 .then(resposta => commit(DEFINIR_TAREFAS, resposta.data));
---
+++ src/stores/modulos/projeto/index.ts
@@ -0,0 +1,53 @@
+import clienteHttp from "@/http";
+import ProjetoInterface from "@/interfaces/ProjetoInterface";
+import { EstadoInterface } from "@/stores";
+import { OBTER_PROJETOS, CADASTRAR_PROJETO, ALTERAR_PROJETO, REMOVER_PROJETO } from "@/stores/tipos-de-acoes";
+import { DEFINIR_PROJETOS, EXCLUI_PROJETO, ADICIONA_PROJETO, ALTERA_PROJETO } from "@/stores/tipos-de-mutacoes";
+import { AxiosResponse } from "axios";
+import { Module } from "vuex";
+
+export interface EstadoProjeto {
+    projetos: Array<ProjetoInterface>;
+}
+
+const projeto: Module<EstadoProjeto, EstadoInterface> = {
+    actions: {
+        [OBTER_PROJETOS]({ commit }) {
+            clienteHttp.get("projetos")
+                .then(resposta => commit(DEFINIR_PROJETOS, resposta.data));
+        },
+        [CADASTRAR_PROJETO](contexto, nomeDoProjeto: string): Promise<AxiosResponse> {
+            return clienteHttp.post("/projetos", {
+                nome: nomeDoProjeto,
+            });
+        },
+        [ALTERAR_PROJETO](contexto, projeto: ProjetoInterface): Promise<AxiosResponse> {
+            return clienteHttp.put(`/projetos/${projeto.id}`, projeto);
+        },
+        [REMOVER_PROJETO]({ commit }, idDoProjeto: string) {
+            return clienteHttp.delete(`/projetos/${idDoProjeto}`)
+                .then(() => commit(EXCLUI_PROJETO, idDoProjeto));
+        },
+    },
+    mutations: {
+        [ADICIONA_PROJETO](state, nomeDoProjeto: string): void {
+            const projeto = {
+                id: new Date().toISOString(),
+                nome: nomeDoProjeto
+            } as ProjetoInterface;
+            state.projetos.push(projeto);
+        },
+        [ALTERA_PROJETO](state, projetoAlterado: ProjetoInterface): void {
+            const index = state.projetos
+                .findIndex(projeto => projeto.id == projetoAlterado.id);
+            state.projetos[index] = projetoAlterado;
+        },
+        [EXCLUI_PROJETO](state, idDoProjeto: string): void {
+            state.projetos = state.projetos
+                .filter(projeto => projeto.id != idDoProjeto);
+        },
+        [DEFINIR_PROJETOS](state, projetos: Array<ProjetoInterface>): void {
+            state.projetos = projetos;
+        },
+    },
+}
